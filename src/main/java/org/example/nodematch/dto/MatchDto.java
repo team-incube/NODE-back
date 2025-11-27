c5667ebea52aa3a07ae2660e5f17d19528e439af
이하경 <s25048@gsm.hs.kr>
2025-11-27 20:16:49 +0900

    1차 수정 완료

--- src/main/java/org/example/nodematch/dto/MatchDto.java
+++ src/main/java/org/example/nodematch/dto/MatchDto.java
@@ -1,41 +1,25 @@
 package org.example.nodematch.dto;
 
-import mport jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 public class MatchingDto {
-
 
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
-    public class MatchingRequestDto {
+    public static class MatchingRequestDto {
+
         @NotBlank
         private String applicantName;
-
-
         private String mbti;
-
-
         private String major;
-
-
         private String desiredMentor;
-
-
         private String reasonToJoinGSM;
-
-
         private String futureGoals;
-
-
         private String worriesBeforeSchool;
-
-
         private String recentConcerns;
-
-
         private String freeMessage;
     }
 }
--- src/main/java/org/example/nodematch/entity/Match.java
+++ src/main/java/org/example/nodematch/entity/Match.java
@@ -1,14 +1,67 @@
 package org.example.nodematch.entity;
 
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class Match {
-
-
-
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.Builder;
 
 
+@Entity
+@Table(name = "infor")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class Infor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(length = 10, nullable = false)
+    private String mbti;
+
+    @Column(length = 50, nullable = false)
+    private String major;
+
+    @Column(length = 50)
+    private String desiredMentor;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String reasonToJoinGSM;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String futureGoals;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String worriesBeforeSchool;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String recentConcerns;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String freeMessage;
+
+
+    @Builder
+    public Infor(String mbti, String major, String desiredMentor,
+                 String reasonToJoinGSM, String futureGoals,
+                 String worriesBeforeSchool, String recentConcerns,
+                 String freeMessage) {
+        this.mbti = mbti;
+        this.major = major;
+        this.desiredMentor = desiredMentor;
+        this.reasonToJoinGSM = reasonToJoinGSM;
+        this.futureGoals = futureGoals;
+        this.worriesBeforeSchool = worriesBeforeSchool;
+        this.recentConcerns = recentConcerns;
+        this.freeMessage = freeMessage;
+    }
 }
