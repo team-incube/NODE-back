3f543e3a2d8e0805479979c28b30a7249d2aa912
user <sooooooobinn@gmail.com>
2025-12-02 19:42:30 +0900

    오류 수정

--- src/main/java/com/example/nodelogin/jwt/LoginFilter.java
+++ src/main/java/com/example/nodelogin/jwt/LoginFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,7 +15,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.Map;
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
