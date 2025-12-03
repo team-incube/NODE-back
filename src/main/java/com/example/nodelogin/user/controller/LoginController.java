e2563baf171091d5a08e87a29aa9231766045e63
user <sooooooobinn@gmail.com>
2025-12-03 17:54:35 +0900

    refactor: LoginController 파일 수정

--- src/main/java/com/example/nodelogin/user/controller/LoginController.java
+++ src/main/java/com/example/nodelogin/user/controller/LoginController.java
@@ -4,10 +4,16 @@ import com.example.nodelogin.jwt.JWTUtil;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.nodelogin.user.dto.LoginDto;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -22,5 +28,24 @@ public class LoginController {
 
     @PostMapping
     public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
+        try{
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword())
+            );
+
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            String accessToken = jwtUtil.generateToken(userDetails);
+            String refreshToken = jwtUtil.generateRefreshToken(userDetails);
+
+            return ResponseEntity.ok(
+                    Map.of(
+                            "accessToken", accessToken,
+                            "refreshToken", refreshToken,
+                            "email", userDetails.getUsername()
+                    )
+            );
+        }catch (Exception e) {
+            return ResponseEntity.status(401).body(Map.of("error", "Invalid email or password()");
+        }
     }
 }
