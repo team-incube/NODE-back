b2f5b9a71afbd0f8f3686e99bdb7610ad67c16c3
user <sooooooobinn@gmail.com>
2025-12-03 17:41:01 +0900

    feat: LoginController 추가

--- src/main/java/com/example/nodelogin/user/controller/JoinController.java
+++ src/main/java/com/example/nodelogin/user/controller/JoinController.java
@@ -4,9 +4,11 @@ import com.example.nodelogin.user.dto.JoinDto;
 import com.example.nodelogin.user.service.JoinService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class JoinController {
 
     private final JoinService joinService;
---
+++ src/main/java/com/example/nodelogin/user/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.example.nodelogin.user.controller;
+
+import com.example.nodelogin.jwt.JWTUtil;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class LoginController {
+    private AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
+
+    public LoginController(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
+    }
+}
